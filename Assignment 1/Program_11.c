f02922644a82d22fd4593cff41e8e18ac71bea9f
Nitin Kumar <nitinkumar20062010@gmail.com>
2026-06-12 11:57:12 +0530

    new

--- Assignment 1/Program_11.c
+++ Assignment 1/Program_11.c
@@ -3,20 +3,5 @@
 #include<stdio.h>
 
 void main(){
-	int num1,num2;
-	
-	printf("\nEnter First Number : ");
-	scanf("%d",&num1);
-	
-	printf("\nEnter Second Number : ");
-	scanf("%d",&num2);
-	
-	printf("\nBefore Swap => First number %d :",num1);
-	printf("Second Number %d ",num2);
-	
-	num1,num2 = num2,num1;
-	
-	printf("\nAfter Swap => First number %d : ",num1);
-	printf("Second Number %d",num2);
 	
 }
